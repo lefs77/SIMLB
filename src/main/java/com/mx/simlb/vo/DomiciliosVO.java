3d5b7dcea21038873d59ceb785b4795a04cc4a0c
PhilipLuis <fhp.3858@gmail.com>
2019-09-13 20:08:23 -0500

    testing commi...

--- src/main/java/com/mx/simlb/vo/DomiciliosVO.java
+++ src/main/java/com/mx/simlb/vo/DomiciliosVO.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class DomiciliosVO implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Long idCatDomicilio;
 	private String colonia;
 	private String calle;
