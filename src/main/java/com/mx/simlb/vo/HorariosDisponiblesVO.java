6ac59369d85aed205e3dcb0777a8079c0ef0e422
PhilipLuis <fhp.3858@gmail.com>
2019-09-13 20:16:52 -0500

    test2

--- src/main/java/com/mx/simlb/vo/HorariosDisponiblesVO.java
+++ src/main/java/com/mx/simlb/vo/HorariosDisponiblesVO.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class HorariosDisponiblesVO implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Long idHorariosDisponibles;
 	private String horario;
 	
